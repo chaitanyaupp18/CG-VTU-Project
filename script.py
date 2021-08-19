8b16f79c7cd109b33c0e6d00dee3d4812e5081c7
Chaitanya M A <43720440+chaitanyaupp18@users.noreply.github.com>
2021-08-20 00:42:51 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt 
   
 # Read Images 
-img = mpimg.imread('/Users/chaitanya/pil_text.png') 
+img = mpimg.imread('pil_text.png') 
 # Output Images 
 plt.imshow(img)
 plt.waitforbuttonpress()
